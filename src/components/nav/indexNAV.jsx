9e23805068da66508cf32da1331ae6810e2406b2
yeezynico <yeezy77680@gmail.com>
2024-05-24 19:18:47 +0200

    jour/nuit

--- src/components/nav/indexNAV.jsx
+++ src/components/nav/indexNAV.jsx
@@ -1,18 +1,30 @@
+// Nav.jsx
+
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import '../../index.scss';
 
 const Nav = () => {
+    const [isNightMode, setIsNightMode] = useState(false);
+
+    const toggleTheme = () => {
+        setIsNightMode(!isNightMode);
+        const html = document.getElementsByTagName('html')[0];
+        html.classList.toggle('nuit');
+    };
 
     return (
         <>
-            <Link to="/restaurants">restaurant </Link>
-
-            <Link to="/">Home </Link>
-    
-            <Link to="/log">Log </Link>
-
-
+            <div className="NAV" >
+                <Link to="/restaurants"><p className='gold'>Restaurant</p></Link>
+                <Link to="/"><p className='gold'>Home</p></Link>
+                <Link to="/log"><p className='gold'>Log</p></Link>
+                <button id="themeLogo" onClick={toggleTheme}>
+                    {isNightMode ? '☀️' : '🌙'}
+                </button>
+            </div>
         </>
-    )
+    );
 }
 
 export default Nav;
